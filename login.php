87be46fb6d2fc484599bd8e4fdfb78aa73b5ea7e
AkhileshNegi <akhileshnegi.an3@gmail.com>
2019-04-03 00:03:40 +0530

    QR working

--- login.php
+++ login.php
@@ -1,28 +1,23 @@
 <?php
-$f = "visit.php";
-if(!file_exists($f)){
-  touch($f);
-  $handle =  fopen($f, "w" ) ;
-  fwrite($handle,0) ;
-  fclose ($handle);
-}
- 
 include('libs/phpqrcode/qrlib.php'); 
-
 function getUsernameFromEmail($email) {
   $find = '@';
   $pos = strpos($email, $find);
   $username = substr($email, 0, $pos);
   return $username;
 }
-
 if(isset($_POST['submit']) ) {
   $tempDir = 'temp/'; 
   $email = $_POST['mail'];
   $subject =  $_POST['subject'];
   $filename = getUsernameFromEmail($email);
   $body =  $_POST['msg'];
-  $codeContents = 'mailto:'.$email.'?subject='.urlencode($subject).'&body='.urlencode($body); 
+  $rating = "5star";
+$codeContents = 'Email:'.$email."\n"; 
+$codeContents .= 'Subject:'.$subject."\n"; 
+$codeContents .= 'Body:'.$body."\n"; 
+$codeContents .= 'Star:'.$rating."\n"; 
+  // $codeContents = 'mailto:'.$email.'?subject='.urlencode($subject).'&body='.urlencode($body); 
   QRcode::png($codeContents, $tempDir.''.$filename.'.png', QR_ECLEVEL_L, 5);
 }
 ?>
