af61456579a99474f0a8c9e9c71f5a90b98d3189
AkhileshNegi <akhileshnegi.an3@gmail.com>
2019-04-02 22:31:08 +0530

    created separate columns for input and output

--- login_page.php
+++ login_page.php
@@ -25,8 +25,51 @@
 <div class="container-fluid">
   <h2>More Equal Columns</h2>
   <div class="row">
-    <div class="col bg-success">1 of 2</div>
-    <div class="col bg-warning">2 of 2</div>
+    <div class="col bg-success">
+      <div class="myoutput">
+        <h3><strong>Quick Response (QR) Code Generator</strong></h3>
+        <div class="input-field">
+          <h3>Please Fill-out All Fields</h3>
+          <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" >
+            <div class="form-group">
+              <label>Email</label>
+              <input type="email" class="form-control" name="mail" style="width:20em;" placeholder="Enter your Email" value="<?php echo @$email; ?>" required />
+            </div>
+            <div class="form-group">
+              <label>Subject</label>
+              <input type="text" class="form-control" name="subject" style="width:20em;" placeholder="Enter your Email Subject" value="<?php echo @$subject; ?>" required pattern="[a-zA-Z .]+" />
+             </div>
+            <div class="form-group">
+            <label>Message</label>
+            <input type="text" class="form-control" name="msg" style="width:20em;" value="<?php echo @$body; ?>" required pattern="[a-zA-Z0-9 .]+" placeholder="Enter your message"></textarea>
+            </div>
+            <div class="form-group">
+              <input type="submit" name="submit" class="btn btn-primary submitBtn" style="width:20em; margin:0;" />
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+    <div class="col bg-warning">
+      <?php
+      if(!isset($filename)){
+        $filename = "author";
+      }
+      ?>
+      <div class="qr-field">
+        <h2 style="text-align:center">QR Code Result: </h2>
+        <center>
+          <div class="qrframe" style="border:2px solid black; width:210px; height:210px;">
+              <?php echo '<img src="temp/'. @$filename.'.png" style="width:200px; height:200px;"><br>'; ?>
+          </div>
+          <a class="btn btn-primary submitBtn" style="width:210px; margin:5px 0;" href="download.php?file=<?php echo $filename; ?>.png ">Download QR Code</a>
+        </center>
+      </div>
+      <div class = "dllink" style="text-align:center;margin:-100px 0px 50px 0px;">
+        <h4>Developer: Tarique Akhtar Ansari</h4>
+      </div>
+    </div>
+    </div>
   </div>
 </div>
 
