a5c5f31d3b0f26fdfe3c7732708c9ea034e31566
AkhileshNegi <akhileshnegi.an3@gmail.com>
2019-04-02 22:22:24 +0530

    added a new temporary login page

---
+++ login_page.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+</head>
+<body>
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <a class="navbar-brand" href="#">Logo</a>  
+  <ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link" href="#">Link 1</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Link 2</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Link 3</a>
+    </li>
+  </ul>
+</nav>
+
+<div class="container-fluid">
+  <h2>More Equal Columns</h2>
+  <div class="row">
+    <div class="col bg-success">1 of 2</div>
+    <div class="col bg-warning">2 of 2</div>
+  </div>
+</div>
+
+</body>
+</html>
