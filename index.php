0535b87383934ed2c3a50bf6fc56fc5cdbab5c5b
Rajat52 <rajatbatoe52@gmail.com>
2019-04-04 00:37:46 +0530

    bg

--- index.php
+++ index.php
@@ -9,7 +9,8 @@
 </head>
 <body>
   <nav class="navbar navbar-expand-lg navbar-light bg-white">
-  <a class="navbar-brand" style="font-family: verdana;" href="#">Navbar</a>
+  <a class="navbar-brand" style="font-family:ARIZONIA;font-size:35px;"
+   >Navbar</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -30,11 +31,12 @@
     </ul>
   </div>
 </nav>
-  <div id="carouselExampleSlidesOnly" class="carousel slide"  data-interval="1500" data-ride="carousel">
-  <div class="carousel-inner" style="padding:30px">
+  <div id="carouselExampleSlidesOnly" class="carousel slide" style="padding:30px;background-color:#008080"  data-interval="1500" data-ride="carousel">
+  <div class="carousel-inner" >
     <div class="carousel-item active">
       <img class="d-block w-100" src="images/3.jpg" alt="First slide">
     </div>
+    
     <div class="carousel-item">
       <img class="d-block w-100" src="images/2.jpg" alt="Second slide">
     </div>
