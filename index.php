2e28dbb133b50ef061dc1c1a85951f4220d7481c
Rajat52 <rajatbatoe52@gmail.com>
2019-04-11 02:03:16 +0530

    button slides

--- index.php
+++ index.php
@@ -7,36 +7,26 @@
   <script src="jquery/jquery.min.js"></script>
   <script src="bootstrap/js/bootstrap.min.js"></script>
 </head>
-<body>
-  <nav class="navbar navbar-expand-lg navbar-light bg-white">
-  <a class="navbar-brand" style="font-family:ARIZONIA;font-size:35px;"
-   >Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+<body >
+ <nav class="navbar navbar-expand-lg " style="background-color:rgb(0, 0, 0)">
+  <a class="navbar-brand nav-text-light" style="font-size:60px;font-family:bold;color:aliceblue;"  >CIESZYC-2019</a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" style="font: bold;" href="#">Features</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Pricing</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
-      </li>
-    </ul>
+  <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+    <div class="navbar-nav">
+      <a class="nav-item nav-link active" style="font-size:25px;color:cadetblue" href="#">Home <span class="sr-only">(current)</span></a>
+      <a class="nav-item nav-link" style=" font-size:25px;color:cadetblue" href="#">Features</a>
+      <a class="nav-item nav-link" style=" font-size:25px;color:cadetblue" href="#">Pricing</a>
+      <a class="nav-item nav-link disabled" style=" font-size:25px;color:cadetblue" href="#">Disabled</a>
+    </div>
   </div>
 </nav>
-  <div id="carouselExampleSlidesOnly" class="carousel slide" style="padding:30px;background-color:#008080"  data-interval="1500" data-ride="carousel">
-  <div class="carousel-inner" >
+<div id="carouselExampleControls" class="carousel slide" style="padding:120px;background-color:rgba(255, 99, 71, 0.4);" data-ride="carousel">
+  <div class="carousel-inner">
     <div class="carousel-item active">
       <img class="d-block w-100" src="images/3.jpg" alt="First slide">
     </div>
-    
     <div class="carousel-item">
       <img class="d-block w-100" src="images/2.jpg" alt="Second slide">
     </div>
@@ -44,7 +34,31 @@
       <img class="d-block w-100" src="images/1.jpg" alt="Third slide">
     </div>
   </div>
+  <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
 </div>
+  
+  <div class="container">
+  <div class="row" style="padding-top:50px;padding-left:100px">
+    <div class="col-sm">
+     
+     
+    </div><br>
+    <div class="col-sm">
+     
+     
+  </div>
+</div>
+
+
+
+    
 
 </body>
 </html>
