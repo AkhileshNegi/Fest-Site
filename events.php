aeb3b81664f3faa5049404bacf5369062e502ce8
AkhileshNegi <akhileshnegi.an3@gmail.com>
2019-04-10 19:06:23 +0530

    using bootstrap cards to display events

---
+++ events.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
+	<title>Events</title>
+</head>
+<body>
+
+<nav class="navbar navbar-expand-sm bg-dark navbar-dark">
+  <a class="navbar-brand" href="#">CIEZYC</a>
+  <ul class="navbar-nav">
+    <li class="nav-item">
+      <a class="nav-link" href="#">Login</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Decoder</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="events.php">Events</a>
+    </li>
+  </ul>
+</nav>
+
+<div class="container-fluid">
+  <h2>EVENTS</h2>
+	<div class="container">
+	  <div class="row d-flex justify-content-center">
+	    <div class="col-sm-3 p-1 m-1">
+	      <div class="card" style="height:400px" >
+    <img class="img-thumbnail" style="height:200px" src="Robo.jpg" alt="Card image">
+    <div class="card-body">
+      <h4 class="card-title">Robo Ralley</h4>
+      <p class="card-text">Fastest to the finish takes it all!!</p>
+      <a href="#" class="btn btn-primary">See Profile</a>
+    </div>
+  </div>
+	    </div>
+	    <div class="col-sm-3 p-1 m-1">
+	      <div class="card" style="height:400px" >
+    <img class="img-thumbnail" style="height:200px" src="csgo.jpg" alt="Card image">
+    <div class="card-body">
+      <h4 class="card-title">CS:GO</h4>
+      <p class="card-text">Classic Terrorist v/s Counter Terrorist Matches</p>
+      <a href="#" class="btn btn-primary">See Profile</a>
+    </div>
+  </div>
+	    </div>
+	    <div class="col-sm-3 p-1 m-1">
+	      <div class="card" style="height:400px" >
+    <img class="img-thumbnail" style="height:200px" src="dota2.jpg" alt="Card image">
+    <div class="card-body">
+      <h4 class="card-title">Dota2</h4>
+      <p class="card-text">5 vs 5 Heroes Battle to Defend their Ancients</p>
+      <a href="#" class="btn btn-primary">See Profile</a>
+    </div>
+  </div>
+	    </div>
+	  </div>
+	</div>
+</div>
+
+	<script src="jquery/jquery.min.js"></script>
+	<script src="bootstrap/js/bootstrap.min.js"></script>
+</body>
+</html>
