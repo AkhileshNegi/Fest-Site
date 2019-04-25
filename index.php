4b0f368010e1c0a84e90d0d9cc107b3e28ecc141
Rajat52 <rajatbatoe52@gmail.com>
2019-04-26 00:23:09 +0530

    timeline etc

--- index.php
+++ index.php
@@ -8,20 +8,22 @@
   <script src="bootstrap/js/bootstrap.min.js"></script>
 </head>
 <body >
- <nav class="navbar navbar-expand-lg " style="background-color:rgb(0, 0, 0)">
-  <a class="navbar-brand nav-text-light" style="font-size:60px;font-family:bold;color:aliceblue;"  >CIESZYC-2019</a>
+  <nav class="navbar navbar-expand-lg" style="background-color:;">
+  <a class="navbar-brand nav-text-light"><img src="images/logo1.png" ></a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
   <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
     <div class="navbar-nav">
-      <a class="nav-item nav-link active" style="font-size:25px;color:cadetblue" href="#">Home <span class="sr-only">(current)</span></a>
+     <a class="nav-item nav-link active" style="font-size:25px;color:cadetblue" href="#">Home <span class="sr-only">(current)</span></a>
       <a class="nav-item nav-link" style=" font-size:25px;color:cadetblue" href="#">Features</a>
       <a class="nav-item nav-link" style=" font-size:25px;color:cadetblue" href="#">Pricing</a>
       <a class="nav-item nav-link disabled" style=" font-size:25px;color:cadetblue" href="#">Disabled</a>
     </div>
   </div>
+  
 </nav>
+ 
 <div id="carouselExampleControls" class="carousel slide" style="padding:120px;background-color:rgba(255, 99, 71, 0.4);" data-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
@@ -41,20 +43,229 @@
   <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="sr-only">Next</span>
-  </a>
+  </a><br>
 </div>
   
-  <div class="container">
-  <div class="row" style="padding-top:50px;padding-left:100px">
-    <div class="col-sm">
-     
-     
-    </div><br>
+   <div  style="padding-top:50px;background-color:rgb(174, 241, 7);">
+    <div class="text-center" style="font-style: bold">
+  <h1 >CELEBRITY</h1><br><br>
+<div class="row">
+  <div class="col">
+    <img src="images/6.jpg" style=" vertical-align: middle;
+  width: 450px;
+  height: 450px;
+  border-radius: 150%;" alt="Avatar" class="avatar">
+<h5>paarth</h5></div>
+  <div class="col">
+   <img src="images/8.jpg" style=" vertical-align: middle;
+  width: 450px;
+  height: 450px;
+  border-radius: 150%;" alt="Avatar" class="avatar">
+<h5>akash</h5>
+</div>
+  </div>
+</div><br><br>
+</div>
+  <div  style="padding-top:50px;padding-left:100px;background-color:rgb(195, 155, 211);">
+    <div class="text-center" style="font-style:bold">
+  <h1 >Our Timeline</h1>
+  <div class="row">
+  <div class="col-sm">
+    <h3>dj night</h3>
+  <a href="#">
+    <img src="images/6.jpg" style="width:100%">
+    </a>
+    
+  </div>
+  <div class="col-sm">
+    <h3>fascino</h3>
+  <a href="#">
+    <img src="images/7.jpg" style="width:100%">
+    </a>
+   
+    </div>
+  <div class="col-sm">
+    <h3>technical</h3>
+    <a href="#">
+    <img src="images/8.jpg" style="width:100%">
+    </a>
+  </div>
+</div>
+</div><br>
+</div>
+  <div  style="padding-top:50px;background-color:lightblue">
+    <div class="text-center" style="font-style: bold">
+  <h1 >ABOUT</h1>
+</div>
+<div class="container">
+  <h5>Cieszyc, the college festival, is an opportunity
+to serve the purpose of exhilarating
+innovative ideas into the students up to a
+great extent. A competition between the
+best minds existing in or around the college
+will allow the best of the ideas to come
+out. The purpose of doing Cieszyc is:
+Firstly, it will help the institution to bring
+together the students from across the
+country. Secondly, fest will promote
+innovative ideas and allow students to
+share their knowledge and ideas with
+other students. Thirdly, attending the fest
+will give information on the different fields
+of study. Events, quizzes, seminars, music
+and dance are the part of the college fest.</h5><br><br>
+</div>
+</div>
+</div><br>
     <div class="col-sm">
      
      
   </div>
 </div>
+<!-- Footer -->
+<footer class="page-footer font-small unique-color-dark">
+
+    <div style="background-color: #6351ce;">
+      <div class="container">
+
+        <!-- Grid row-->
+        <div class="row py-4 d-flex align-items-center">
+
+          <!-- Grid column -->
+          <div class="col-md-6 col-lg-5 text-center text-md-left mb-4 mb-md-0">
+            <h6 class="mb-0">Get connected with us on social networks!</h6>
+          </div>
+          <!-- Grid column -->
+
+          <!-- Grid column -->
+          <div class="col-md-6 col-lg-7 text-center text-md-right">
+
+            <!-- Facebook -->
+            <a class="fb-ic">
+              <i class="fab fa-facebook-f white-text mr-4"> </i>
+            </a>
+            <!-- Twitter -->
+            <a class="tw-ic">
+              <i class="fab fa-twitter white-text mr-4"> </i>
+            </a>
+            <!-- Google +-->
+            <a class="gplus-ic">
+              <i class="fab fa-google-plus-g white-text mr-4"> </i>
+            </a>
+            <!--Linkedin -->
+            <a class="li-ic">
+              <i class="fab fa-linkedin-in white-text mr-4"> </i>
+            </a>
+            <!--Instagram-->
+            <a class="ins-ic">
+              <i class="fab fa-instagram white-text"> </i>
+            </a>
+
+          </div>
+          <!-- Grid column -->
+
+        </div>
+        <!-- Grid row-->
+
+      </div>
+    </div>
+
+    <!-- Footer Links -->
+    <div class="container text-center text-md-left mt-5">
+
+      <!-- Grid row -->
+      <div class="row mt-3">
+
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-4 col-xl-3 mx-auto mb-4">
+
+          <!-- Content -->
+          <h6 class="text-uppercase font-weight-bold">Company name</h6>
+          <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+          <p>Here you can use rows and columns here to organize your footer content. Lorem ipsum dolor sit amet, consectetur
+            adipisicing elit.</p>
+
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-2 col-lg-2 col-xl-2 mx-auto mb-4">
+          <script src="https://code.jquery.com/jquery-3.3.1.js"></script>
+
+
+          <!-- Links -->
+          <h6 class="text-uppercase font-weight-bold">Products</h6>
+          <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+          <p>
+            <a href="#!">MDBootstrap</a>
+          </p>
+          <p>
+            <a href="#!">MDWordPress</a>
+          </p>
+          <p>
+            <a href="#!">BrandFlow</a>
+          </p>
+          <p>
+            <a href="#!">Bootstrap Angular</a>
+          </p>
+
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-3 col-lg-2 col-xl-2 mx-auto mb-4">
+
+          <!-- Links -->
+          <h6 class="text-uppercase font-weight-bold">Useful links</h6>
+          <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+          <p>
+            <a href="#!">Your Account</a>
+          </p>
+          <p>
+            <a href="#!">Become an Affiliate</a>
+          </p>
+          <p>
+            <a href="#!">Shipping Rates</a>
+          </p>
+          <p>
+            <a href="#!">Help</a>
+          </p>
+
+        </div>
+        <!-- Grid column -->
+
+        <!-- Grid column -->
+        <div class="col-md-4 col-lg-3 col-xl-3 mx-auto mb-md-0 mb-4">
+
+          <!-- Links -->
+          <h6 class="text-uppercase font-weight-bold">Contact</h6>
+          <hr class="deep-purple accent-2 mb-4 mt-0 d-inline-block mx-auto" style="width: 60px;">
+          <p>
+            <i class="fas fa-home mr-3"></i> New York, NY 10012, US</p>
+          <p>
+            <i class="fas fa-envelope mr-3"></i> info@example.com</p>
+          <p>
+            <i class="fas fa-phone mr-3"></i> + 01 234 567 88</p>
+          <p>
+            <i class="fas fa-print mr-3"></i> + 01 234 567 89</p>
+
+        </div>
+        <!-- Grid column -->
+
+      </div>
+      <!-- Grid row -->
+
+    </div>
+    <!-- Footer Links -->
+
+    <!-- Copyright -->
+    <div class="footer-copyright text-center py-3">CIESZYC 2019
+      <a href="bootstrap/css/bootstrap-reboot.css"> </a>
+    </div>
+    
+
+  </footer>
+  
 
 
 
